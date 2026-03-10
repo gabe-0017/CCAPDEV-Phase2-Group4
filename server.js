9845cb09cc02a7e32b0fbe45e945c79d34b58f40
gabe-0017 <john_gabriel_calderon@dlsu.edu.ph>
2026-03-10 22:41:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+const userController = require("./controllers/userController");
+const labController = require("./controllers/labController");
+const reservationController = require("./controllers/reservationController");
+
+// user routes
+app.post("/register", userController.registerUser);
+app.post("/login", userController.loginUser);
+app.get("/profile/:id", userController.getUserProfile);
+app.get("/search", userController.searchUsers);
+
+
+// lab routes
+app.get("/labs", labController.getLabs);
+app.post("/labs/create", labController.createLab);
+
+
+// reservation routes
+app.post("/reserve", reservationController.createReservation);
+app.get("/reservations", reservationController.getReservations);
+app.delete("/reservation/:id", reservationController.cancelReservation);
+app.put("/reservation/:id", reservationController.editReservation);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
